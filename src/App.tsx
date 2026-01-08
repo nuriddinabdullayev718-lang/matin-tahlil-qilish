4e229a12acff1e41e6a07c7b8d289e30a86625df
nuriddinabdullayev718-lang <nuriddinabdullayev718@gmail.com>
2026-01-08 22:08:23 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Matn tahlil qilish tizimi</h1>
+      <p>Ilova muvaffaqiyatli ishga tushdi ✅</p>
+    </div>
+  );
+}
