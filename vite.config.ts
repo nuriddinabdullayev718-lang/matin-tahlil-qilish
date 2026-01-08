39950a3dbc7f40e29b7f323f38ab201339fdbf68
nuriddinabdullayev718-lang <nuriddinabdullayev718@gmail.com>
2026-01-08 21:53:30 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,19 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: path.resolve(__dirname, "client"),
+
   plugins: [react()],
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "client", "src"),
       "@shared": path.resolve(__dirname, "shared"),
     },
   },
 
   build: {
-    outDir: "dist",
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
 });
