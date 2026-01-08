3c94253869fd492f93712cb8a2c9d78b36cbcfa9
nuriddinabdullayev718-lang <nuriddinabdullayev718@gmail.com>
2026-01-08 23:55:04 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// === __dirname (ESM uchun TO‘G‘RI) ===
+// === __dirname (ESM uchun to‘g‘ri) ===
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
